cba8bfaa3187a7c0646c63655d3de249f956055e
Daniel Tschumi <4465702+tschumi@users.noreply.github.com>
2025-12-08 18:19:06 +0000

    Fix dist/server.cfg getting overwritten

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-[[ -n $RCON_PASSWORD ]] && RCON_PASSWORD="rcon_password $RCON_PASSWORD"
-[[ -n $WINLIMIT ]] && WINLIMIT="mp_winlimit $WINLIMIT"
-[[ -n $SV_PASSWORD ]] && SV_PASSWORD="sv_password $SV_PASSWORD"
 [[ -z $MAP ]] && MAP="dm_underpass"
 [[ -z $MAXPLAYERS ]] && MAXPLAYERS=8
 
@@ -13,12 +10,45 @@ else
 	SV_LAN="+sv_lan 1"
 fi
 
-cat <<EOF >/steam/hl2dm/hl2mp/cfg/server.cfg
-hostname "$SV_HOSTNAME"
-$RCON_PASSWORD
-$SV_PASSWORD
-$WINLIMIT
-$MAPCYCLE_FILE
-EOF
+# Replace values in the existing server.cfg
+SERVER_CFG="/steam/hl2dm/hl2mp/cfg/server.cfg"
+
+if [[ -f "$SERVER_CFG" ]]; then
+    # Replace hostname if SV_HOSTNAME is set
+    [[ -n $SV_HOSTNAME ]] && sed -i "s/^hostname \".*\"/hostname \"$SV_HOSTNAME\"/" "$SERVER_CFG"
+    
+    # Replace sv_password if SV_PASSWORD is set
+    if [[ -n $SV_PASSWORD ]]; then
+        if grep -q "^sv_password" "$SERVER_CFG"; then
+            sed -i "s/^sv_password \".*\"/sv_password \"$SV_PASSWORD\"/" "$SERVER_CFG"
+        else
+            # Add it after the hostname line if it doesn't exist
+            sed -i "/^hostname/a sv_password \"$SV_PASSWORD\"" "$SERVER_CFG"
+        fi
+    fi
+    
+    # Replace or add rcon_password if RCON_PASSWORD is set
+    if [[ -n $RCON_PASSWORD ]]; then
+        if grep -q "^rcon_password" "$SERVER_CFG"; then
+            sed -i "s/^rcon_password .*/rcon_password \"$RCON_PASSWORD/" "$SERVER_CFG"
+        else
+            # Add it after sv_password if it doesn't exist
+            sed -i "/^sv_password/a rcon_password \"$RCON_PASSWORD\"" "$SERVER_CFG"
+        fi
+    fi
+    
+    # Replace or add mp_winlimit if WINLIMIT is set
+    if [[ -n $WINLIMIT ]]; then
+        if grep -q "^mp_winlimit" "$SERVER_CFG"; then
+            sed -i "s/^mp_winlimit .*/mp_winlimit $WINLIMIT/" "$SERVER_CFG"
+        else
+            # Add it in the gameplay settings section
+            sed -i "/^\/\/==ESSENTIAL GAMEPLAY SETTINGS/a mp_winlimit $WINLIMIT" "$SERVER_CFG"
+        fi
+    fi
+    
+    # Handle MAPCYCLE_FILE if set
+    [[ -n $MAPCYCLE_FILE ]] && sed -i "s|^exec mapcycle.*|exec $MAPCYCLE_FILE|" "$SERVER_CFG"
+fi
 
 exec ./srcds_run -game hl2mp $SV_LAN -ip 0.0.0.0 +exec server.cfg +map $MAP -maxplayers $MAXPLAYERS
